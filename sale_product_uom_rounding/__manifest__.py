0348090531163a392ce7e862f46c6bc119fcc607
Karl Southern <karl@theangryangel.co.uk>
2020-08-10 12:19:10 +0100

    Add sale_product_uom_rounding

---
+++ sale_product_uom_rounding/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': "sale_product_uom_rounding",
+
+    'summary': """
+    Round sale order line quantities when entered
+    """,
+
+    'author': "Glo Networks",
+    'website': "https://www.glo.systems/",
+
+    'category': 'Uncategorized',
+    'version': '12.0.1.0.0',
+    'depends': ['sale', 'uom'],
+    'data': [
+    ],
+    'demo': [
+    ],
+}
---
+++ sale_product_uom_rounding/models/models.py
@@ -0,0 +1,21 @@
+from odoo import models, api
+from odoo.tools import float_round, float_compare
+
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+    @api.onchange('product_uom', 'product_uom_qty')
+    def product_uom_change(self):
+        uom = self.product_uom
+        qty = self.product_uom_qty
+
+        if uom and qty:
+            qty = float_round(
+                qty, precision_rounding=uom.rounding, rounding_method='UP'
+            )
+
+            if float_compare(qty, self.product_uom_qty, precision_rounding=uom.rounding) != 0:
+                self.product_uom_qty = qty
+
+        return super(SaleOrderLine, self).product_uom_change()
