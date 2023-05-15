10741b03c366415f9c2c0420cf3b8abfeacfc753
GarethGloSystems <gareth@glo.systems>
2023-05-15 13:02:50 +0100

    [FEAT] Generalise force_manual_delivered_qty from wwuk_sales_order

---
+++ sale_force_manual_delivered/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "sale_forced_manual_delivered_qty",
+    "summary": "Allow forced manual delivery",
+    "author": "Glo Networks",
+    "website": "https://github.com/GlodoUK/sale",
+    "category": "Sales",
+    "version": "15.0.1.0.0",
+    "depends": ["sale"],
+    "data": [
+        "views/sale_order.xml",
+    ],
+    "license": "LGPL-3",
+}
