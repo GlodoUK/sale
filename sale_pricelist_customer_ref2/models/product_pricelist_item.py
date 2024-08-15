dcfc90f8336dabeee522d061187892c00bea4582
Karl <karl@theangryangel.co.uk>
2024-08-15 15:16:40 +0100

    Delete sale_pricelist_customer_ref2 directory

--- sale_pricelist_customer_ref2/models/product_pricelist_item.py
+++
@@ -1,7 +0,0 @@
-from odoo import fields, models
-
-
-class ProductPricelistItem(models.Model):
-    _inherit = "product.pricelist.item"
-
-    customer_ref = fields.Char(string="Customer Reference")
